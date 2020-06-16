777f09b6bfada64ad82f9a4209a7e926082c8036
weishaodaren <zhaojiawei@realwaychina.com>
2020-06-16 13:37:16 +0800

    guess the number

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+// use ferris_says::say;
+// use std::io::{stdout, BufWriter};
+
+// fn main() {
+//     let stdout = stdout();
+//     let message = String::from("Hello fellow Rustaceans with WEISHAODAREN !");
+//     let width = message.chars().count();
+
+//     let mut writer = BufWriter::new(stdout.lock());
+//     say(message.as_bytes(), width, &mut writer).unwrap();
+// }
+use std::io;
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans with WEISHAODAREN !");
-    let width = message.chars().count();
-
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    println!("Guess the number!");
+    println!("Please input your guess!");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    println!("You guessed: {}", guess)
 }
