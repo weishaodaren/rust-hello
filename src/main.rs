8b9eb8b1c64c486b557d1b4c6dc65d8007887c4c
weishaodaren <zhaojiawei@realwaychina.com>
2020-06-17 21:11:38 +0800

    play with secret_num

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_rang(1, 101);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
     println!("The secret number is {}", secret_number);
 
