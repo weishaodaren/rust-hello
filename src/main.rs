bf5c391b681cc01929404a5612cfb5b40d478202
weishaodaren <zhaojiawei@realwaychina.com>
2020-06-18 14:41:40 +0800

    play with loop & break

--- src/main.rs
+++ src/main.rs
@@ -20,21 +20,30 @@ fn main() {
 
     println!("The secret number is {}", secret_number);
 
-    println!("Please input your guess!");
+    loop {
+        println!("Please input your guess");
 
-    let mut guess = String::new();
+        
+        println!("Please input your guess!");
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number");
 
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You win!")
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
+
 }
